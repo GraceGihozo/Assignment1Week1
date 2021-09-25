e6e17a9e528c6741852d1465fc50ebb5cd504a79
Grace Gihozo Mugisha <g.mugisha1@alustudent.com>
2021-09-24 17:43:56 -0700

    Initial commit

---
+++ W1_A1_Q1_Grace Gihozo Mugisha.py
@@ -0,0 +1,25 @@
+# Program that prints your initials in big letters,example, my name is Grace Gihozo Mugisha
+print("G G G G G G G G"        "      "      "G G G G G G G G"         "         "  "M                   M"                     )
+print("G"                      "      "   "    "  "          ""G"      "                       "  "M   M            M  M"                  )
+print("G"                      "      "   "    "  "          ""G"      "                       "  "M      M        M   M"                 )
+print("G"                      "      "   "    "  "          ""G"      "                       "  "M       M     M     M"               )
+print("G"                      "      "   "    "  "          ""G"      "                       "  "M          M        M"               )
+print("G"                      "      "   "    "  "          ""G"      "                       "  "M                   M"              )
+print("G        G G  G"        "      "       "G        G G  G"        "         "  "M                   M"             )
+print("G             G"        "      "       "G             G"        "         "  "M                   M"             )
+print("G             G"        "      "       "G             G"        "         "  "M                   M"            )
+print("G G G G G G G G"        "      "       "G G G G G G G G"        "         "  "M                   M"          )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ W1_A1_Q2_Grace Gihozo Mugisha.py
@@ -0,0 +1,12 @@
+# GIVEN
+  # Is that user enter guess secret number.
+# Required
+  #to tell the user whether their number was too large or small.
+  # to tell number of tries
+# Algorithm
+  # import random
+  # n = random.randint(1, 50) guess = int(input("Enter an integer from 1 to 50: "))
+  # while n != "guess"
+  #if guess < n: print ("guess is small" guess= int(input("Enter an integer from 1 to 50: ")) )
+  #elif guess > n: print ("guess is large" guess = int(input("Enter an integer from 1 to 50: ")))
+  #else: print ("you guessed it!")
